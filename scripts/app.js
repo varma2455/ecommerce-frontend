c50bcb85ae93618141248815153f02038da538fc
varma2455 <yeswanthvarma684280@gmail.com>
2026-02-26 21:11:00 +0530

    Initial project setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
-console.log("E-Commerce Website Loaded");
+document.querySelector(".primary").addEventListener("click", () => {
+    document.body.style.transition = "opacity 0.6s ease";
+    document.body.style.opacity = "0";
+  
+    setTimeout(() => {
+      // redirect later
+      alert("Entering ShopEase 🚀");
+      document.body.style.opacity = "1";
+    }, 600);
+  });
+  
+  document.querySelector(".secondary").addEventListener("click", () => {
+    alert("Redirect to Sign In 🔐");
+  });
