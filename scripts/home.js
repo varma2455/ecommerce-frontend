80388dac09db3ee2a69b74f28288d0b38ba66aa5
varma2455 <yeswanthvarma684280@gmail.com>
2026-02-26 23:17:00 +0530

    Initial project setup

---
+++ scripts/home.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger?.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
